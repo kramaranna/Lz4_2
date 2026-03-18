2c5a9d68ae60cbbd87b06327306101f2943f39bf
kramaranna <kramaranna123@gmail.com>
2026-03-18 14:41:28 +0200

    Add files via upload

---
+++ Lz4/Lz4/MyForm.h
@@ -0,0 +1,144 @@
+#pragma once
+#include <cmath>
+
+namespace Lz4 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	protected: 
+
+	private:
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(628, 63);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::SystemColors::MenuHighlight;
+			this->button1->Location = System::Drawing::Point(229, 81);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(148, 59);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Вивести";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(192)), static_cast<System::Int32>(static_cast<System::Byte>(192)), 
+				static_cast<System::Int32>(static_cast<System::Byte>(255)));
+			this->richTextBox1->Location = System::Drawing::Point(195, 146);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(216, 138);
+			this->richTextBox1->TabIndex = 2;
+			this->richTextBox1->Text = L"";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::PapayaWhip;
+			this->ClientSize = System::Drawing::Size(650, 296);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+			 }
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+ // Ініціалізація початкових значень за умовою
+    double an = 1.0; // це a_n
+    double an1 = 3.0; // це a_n+1
+    double an2;        // це a_n+2
+    
+    // Очищення поля перед виведенням
+    richTextBox1->Clear();
+    
+    // Виводимо перші два члени, які нам уже відомі
+    richTextBox1->AppendText("a[0] = " + Convert::ToString(an) + "\r\n");
+    richTextBox1->AppendText("a[1] = " + Convert::ToString(an1) + "\r\n");
+
+    //Цикл для обчислення наступних 9 значень (разом буде 10)
+    for (int n = 0; n < 9; n++) 
+    {
+        // Рекурентна формула
+        double base = (2 * an1) - (3 * an);
+        an2 = std::pow(base, 3) + 2;
+
+        // Виведення результату 
+        richTextBox1->AppendText("a[" + (n + 2) + "] = " + Convert::ToString(an2) + "\r\n");
+
+        // Зсув значень
+        // Те, що було a_n+1, стає a_n
+        // Те, що було a_n+2, стає a_n+1
+        an = an1;
+        an1 = an2;
+    }
+			 }
+	};
+}
